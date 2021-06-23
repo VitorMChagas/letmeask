41b36bcbf37332d16e1ad30a50ebbade1aee84d8
VitorMChagas <vitormchagas@gmail.com>
2021-06-23 12:48:36 -0300

    feat: Added Navigation

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,10 +1,11 @@
+import { Link } from 'react-router-dom';
+
 import illustrationImg from "../assets/images/illustration.svg";
 import logoImg from '../assets/images/logo.svg';
 
-// webpack (snowpack, vite, ...)
+import { Button } from './../components/Button';
 
 import '../styles/auth.scss';
-import { Button } from './../components/Button';
 
 export function NewRoom() {
   return (
@@ -31,7 +32,7 @@ export function NewRoom() {
             </Button>
           </form>
           <p>
-              Quer entrar em uma sala existente? <a href="#">clique aqui</a>
+              Quer entrar em uma sala existente? <Link to="/">clique aqui</Link>
           </p>
         </div>
       </main>
